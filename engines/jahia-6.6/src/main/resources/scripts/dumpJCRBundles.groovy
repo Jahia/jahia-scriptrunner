53f053b4f1cf815058bf07ea281b5661f9898a68
Serge Huber <shuber@jahia.com>
2013-09-30 16:55:55 +0200

    We now have BundleDbPersistenceManager starting up properly ! They now startup with consistency fixes.

--- engines/jahia-6.6/src/main/resources/scripts/dumpJCRBundles.groovy
+++ engines/jahia-6.6/src/main/resources/scripts/dumpJCRBundles.groovy
@@ -1,58 +1,195 @@
 package scripts
 
+import org.apache.commons.io.IOUtils
+import org.apache.jackrabbit.core.NamespaceRegistryImpl
+import org.apache.jackrabbit.core.data.FileDataStore
+import org.apache.jackrabbit.core.fs.FileSystemException
+import org.apache.jackrabbit.core.fs.FileSystemResource
+import org.apache.jackrabbit.core.fs.db.DbFileSystem
+import org.apache.jackrabbit.core.fs.local.LocalFileSystem
+import org.apache.jackrabbit.core.id.NodeId
+import org.apache.jackrabbit.core.nodetype.NodeTypeRegistry
+import org.apache.jackrabbit.core.persistence.PMContext
+import org.apache.jackrabbit.core.persistence.pool.BundleDbPersistenceManager
+import org.apache.jackrabbit.core.util.db.ConnectionFactory
+import org.jahia.server.tools.scriptrunner.engines.common.DatabaseConfiguration
+import org.jdom.Element
+import org.jdom.JDOMException
+import org.jdom.input.SAXBuilder
+import org.jdom.xpath.XPath
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import javax.jcr.RepositoryException
+import javax.sql.DataSource
 import java.sql.Connection
 import java.sql.PreparedStatement
 import java.sql.ResultSet
-import org.jdom.*;
-import org.jdom.output.*;
-import org.jdom.input.*;
 
 Logger logger = LoggerFactory.getLogger("dumpJCRBundles.groovy");
 
 Connection connection = (Connection) jdbcConnection;
+DatabaseConfiguration dbConfiguration = (DatabaseConfiguration) databaseConfiguration;
 
-logger.info("Dumping contents of table jr_default_bundle...");
+// PreparedStatement preparedStatement = connection.prepareStatement("SELECT NODE_ID, BUNDLE_DATA FROM jr_default_bundle");
 
-PreparedStatement preparedStatement = connection.prepareStatement("SELECT NODE_ID, BUNDLE_DATA FROM jr_default_bundle");
+File jahiaInstallLocation = (File) jahiaInstallLocationFile;
 
-ResultSet resultSet = preparedStatement.executeQuery();
+File jackrabbitHomeDir = new File(jahiaInstallLocation, "WEB-INF"+File.separator+"var"+File.separator+"repository");
 
+// ResultSet resultSet = preparedStatement.executeQuery();
+
+Element repositoryXmlRootElement = getRepositoryXmlRootElement(jahiaInstallLocation);
+
+ConnectionFactory connectionFactory = new ConnectionFactory();
+
+DbFileSystem dbFileSystem = getDbFileSystem(repositoryXmlRootElement);
+dbFileSystem.connectionFactory = connectionFactory;
+dbFileSystem.driver = dbConfiguration.driverName;
+dbFileSystem.url = dbConfiguration.connectionURL;
+dbFileSystem.user = dbConfiguration.userName;
+dbFileSystem.password = dbConfiguration.password;
+dbFileSystem.init();
+
+NamespaceRegistryImpl namespaceRegistry = new NamespaceRegistryImpl(dbFileSystem);
+
+NodeTypeRegistry nodeTypeRegistry = new NodeTypeRegistry(namespaceRegistry, dbFileSystem);
+
+FileDataStore fileDataStore = new FileDataStore();
+fileDataStore.init(jackrabbitHomeDir.absolutePath);
+
+LocalFileSystem localDefaultFileSystem = new LocalFileSystem();
+localDefaultFileSystem.path = jackrabbitHomeDir.absolutePath + File.separator + "workspaces" + File.separator + "default";
+
+PMContext defaultPMContext = new PMContext(jackrabbitHomeDir, localDefaultFileSystem, loadRootNodeId(dbFileSystem), namespaceRegistry, nodeTypeRegistry, fileDataStore);
+
+logger.info("Checking default workspace bundles...");
+
+BundleDbPersistenceManager defaultDbPersistenceManager = getPersistenceManagerClass(repositoryXmlRootElement);
+defaultDbPersistenceManager.connectionFactory = connectionFactory;
+defaultDbPersistenceManager.driver = dbConfiguration.driverName;
+defaultDbPersistenceManager.url = dbConfiguration.connectionURL;
+defaultDbPersistenceManager.user = dbConfiguration.userName;
+defaultDbPersistenceManager.consistencyCheck = "true";
+defaultDbPersistenceManager.schemaObjectPrefix = "jr_default_";
+defaultDbPersistenceManager.init(defaultPMContext);
+
+LocalFileSystem localLiveFileSystem = new LocalFileSystem();
+localLiveFileSystem.path = jackrabbitHomeDir.absolutePath + File.separator + "workspaces" + File.separator + "live";
+PMContext livePMContext = new PMContext(jackrabbitHomeDir, localLiveFileSystem, loadRootNodeId(dbFileSystem), namespaceRegistry, nodeTypeRegistry, fileDataStore);
+
+logger.info("Checking live workspace bundles...");
+
+BundleDbPersistenceManager liveDbPersistenceManager = getPersistenceManagerClass(repositoryXmlRootElement);
+liveDbPersistenceManager.connectionFactory = connectionFactory;
+liveDbPersistenceManager.driver = dbConfiguration.driverName;
+liveDbPersistenceManager.url = dbConfiguration.connectionURL;
+liveDbPersistenceManager.user = dbConfiguration.userName;
+liveDbPersistenceManager.consistencyCheck = "true";
+liveDbPersistenceManager.schemaObjectPrefix = "jr_live_";
+liveDbPersistenceManager.init(livePMContext);
+
+LocalFileSystem localVersioningFileSystem = new LocalFileSystem();
+localVersioningFileSystem.path = jackrabbitHomeDir.absolutePath + File.separator + "version";
+
+PMContext versioningPmContext = new PMContext(jackrabbitHomeDir, localVersioningFileSystem, loadRootNodeId(dbFileSystem), namespaceRegistry, nodeTypeRegistry, fileDataStore);
+
+logger.info("Checking versioning bundles...");
+
+BundleDbPersistenceManager versioningDbPersistenceManager = getPersistenceManagerClass(repositoryXmlRootElement);
+versioningDbPersistenceManager.connectionFactory = connectionFactory;
+versioningDbPersistenceManager.driver = dbConfiguration.driverName;
+versioningDbPersistenceManager.url = dbConfiguration.connectionURL;
+versioningDbPersistenceManager.user = dbConfiguration.userName;
+versioningDbPersistenceManager.consistencyCheck = "true";
+versioningDbPersistenceManager.schemaObjectPrefix = "jr_v_";
+versioningDbPersistenceManager.init(versioningPmContext);
+
+/*
 while (resultSet.next()) {
     String nodeId = resultSet.getString(1);
     InputStream bundleData = resultSet.getBinaryStream(2);
-    readBundle(bundleData);
-    logger.info("path=" + path + " name=" + name + " lastModified=" + lastModifiedDate + " length=" + length);
-    if (length > 0) {
-        InputStream data = resultSet.getBinaryStream(3);
-        if (name.toLowerCase().endsWith(".properties")) {
-            Properties properties = new Properties();
-            properties.load(data);
-            for (String propertyName : properties.stringPropertyNames()) {
-                logger.info("--> " + propertyName + " = " + properties.getProperty(propertyName));
-            }
-        } else if (name.toLowerCase().endsWith(".xml")) {
-            if (Boolean.getBoolean("dumpXML").booleanValue()) {
-                fileReader = new InputStreamReader(data);
-                SAXBuilder saxBuilder = new SAXBuilder();
-                Document jdomDocument = saxBuilder.build(fileReader);
-                XMLOutputter xmlOutputter = new XMLOutputter();
-                xmlOutputter.setFormat(Format.getPrettyFormat());
-                xmlOutputter.output(jdomDocument, System.out);
-            } else {
-                logger.info("--> Use option -DdumpXML=true to dump contents of XML file")
-            }
-        }
-        data.close();
-    }
+    readBundle(nodeId, bundleData);
 }
 
 resultSet.close();
 preparedStatement.close();
+*/
 
-private void readBundle(InputStream bundleData) {
-    DataInputStream bundleStream = new DataInputStream(bundleData);
-    int version = bundleStream.readUnsignedByte();
+private void readBundle(String nodeId, InputStream bundleData) {
+
+}
+
+private DataSource getDataSource(ConnectionFactory connectionFactory, String driver, String url, String user, String password) throws Exception {
+    return connectionFactory.getDataSource(driver, url, user, password);
+}
+
+private NodeId loadRootNodeId(org.apache.jackrabbit.core.fs.FileSystem fileSystem) throws RepositoryException {
+    final NodeId ROOT_NODE_ID = NodeId.valueOf("cafebabe-cafe-babe-cafe-babecafebabe");
+
+    try {
+        FileSystemResource uuidFile = new FileSystemResource(
+                fileSystem, "/meta/rootUUID");
+        if (uuidFile.exists()) {
+            // Load uuid of the repository's root node. It is stored in
+            // text format (36 characters) for better readability.
+            InputStream inputStream = uuidFile.getInputStream();
+            try {
+                return NodeId.valueOf(IOUtils.toString(inputStream, "US-ASCII"));
+            } finally {
+                IOUtils.closeQuietly(inputStream);
+            }
+        } else {
+            // Use hard-coded uuid for root node rather than generating
+            // a different uuid per repository instance; using a
+            // hard-coded uuid makes it easier to copy/move entire
+            // workspaces from one repository instance to another.
+            uuidFile.makeParentDirs();
+            OutputStream out = uuidFile.getOutputStream();
+            try {
+                out.write(ROOT_NODE_ID.toString().getBytes("US-ASCII"));
+                return ROOT_NODE_ID;
+            } finally {
+                IOUtils.closeQuietly(out);
+            }
+        }
+    } catch (IOException e) {
+        throw new RepositoryException(
+                "Failed to load or persist the root node identifier", e);
+    } catch (FileSystemException fse) {
+        throw new RepositoryException(
+                "Failed to access the root node identifier", fse);
+    }
+}
+
+public Element getRepositoryXmlRootElement(File jahiaInstallLocation) {
+    SAXBuilder saxBuilder = new SAXBuilder();
+    try {
+        fileReader = new InputStreamReader(new FileInputStream(new File(jahiaInstallLocation, "WEB-INF"+File.separator+"etc" + File.separator + "repository" + File.separator + "jackrabbit" + File.separator + "repository.xml")));
+        org.jdom.Document jdomDocument = saxBuilder.build(fileReader);
+        Element rootElement = jdomDocument.getRootElement();
+        return rootElement;
+    } catch (FileNotFoundException e) {
+        logger.error("Error loading database configuration", e);
+    } catch (JDOMException e) {
+        logger.error("Error loading database configuration", e);
+    } catch (IOException e) {
+        logger.error("Error loading database configuration", e);
+    }
+};
+
+public BundleDbPersistenceManager getPersistenceManagerClass(Element rootElement) {
+    Element persistenceManagerElement = (Element) XPath.newInstance("/Repository/Workspace/PersistenceManager").selectSingleNode(rootElement);
+    String persistenceManagerClassName = persistenceManagerElement.getAttributeValue("class");
+    Class persistenceManagerClass = this.getClass().getClassLoader().loadClass(persistenceManagerClassName);
+    BundleDbPersistenceManager bundleDbPersistenceManager = (BundleDbPersistenceManager) persistenceManagerClass.newInstance();
+    return bundleDbPersistenceManager;
+}
+
+public DbFileSystem getDbFileSystem(Element rootElement) {
+    Element fileSystemElement = (Element) XPath.newInstance("/Repository/FileSystem").selectSingleNode(rootElement);
+    String fileSystemClassName = fileSystemElement.getAttributeValue("class");
+    Class fileSystemClass = this.getClass().getClassLoader().loadClass(fileSystemClassName);
+    DbFileSystem dbFileSystem = (DbFileSystem) fileSystemClass.newInstance();
+    return dbFileSystem;
 }
